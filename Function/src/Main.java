abd740eb0ab2e2718d567a3a8dd3b896611d203a
Aziz <ahotelwala@gmail.com>
2025-06-08 11:28:24 +0530

    Function Commit

---
+++ Function/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+       Scanner in = new Scanner(System.in);
+       System.out.println("Enter number 1:");
+       int num1 = in.nextInt();
+        System.out.println("Enter number 2:");
+        int num2 = in.nextInt();
+        int sum = num1 + num2;
+        System.out.println("The sum====="+sum);
+
+    }
+}
